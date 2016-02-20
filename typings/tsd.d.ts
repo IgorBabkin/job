cb98f57092e9bc78da4c201a6dc601d3567242b4
IgorBabkin <igba14@gmail.com>
2016-02-20 22:20:14 +0200

    remove typings

--- typings/tsd.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="es6-promise/es6-promise.d.ts" />
-/// <reference path="expect.js/expect.js.d.ts" />
-/// <reference path="mocha/mocha.d.ts" />
-/// <reference path="node/node.d.ts" />
