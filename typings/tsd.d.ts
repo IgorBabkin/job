15834e88e9a8c9097753939a7a3fd4d9c288dbba
IgorBabkin <igba14@gmail.com>
2016-02-20 22:00:23 +0200

    add typings to git

---
+++ typings/tsd.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="es6-promise/es6-promise.d.ts" />
+/// <reference path="expect.js/expect.js.d.ts" />
+/// <reference path="mocha/mocha.d.ts" />
+/// <reference path="node/node.d.ts" />
